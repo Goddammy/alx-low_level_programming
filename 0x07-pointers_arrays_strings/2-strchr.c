61cd9c5da70949ae28b0ef6344ba9fc8ed0102fd
goddammy <goddammy@gmail.com>
2022-03-30 08:45:58 -0700

    2-strchr.c

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -1,12 +1,11 @@
 #include "main.h"
 
 /**
- *_strchr - fills memory with a constant byte.
- * @s: pointer to put the constant
- * @c: constnt
- * Return: s
+ *  *_strchr - fills memory with a constant byte.
+ *   @s: pointer to put the constant
+ *    @c: constant
+ *     Return: s
  */
-
 char *_strchr(char *s, char c)
 
 {
