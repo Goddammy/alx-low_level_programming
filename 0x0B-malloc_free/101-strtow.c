00b02083eb1a3159c05eddc5c8f093cd633ab739
goddammy <goddammy@gmail.com>
2022-04-09 08:38:59 -0700

    0-malloc_checked.c

--- 0x0B-malloc_free/101-strtow.c
+++ 0x0B-malloc_free/101-strtow.c
@@ -13,13 +13,10 @@ int wrdcnt(char *s)
 	int i, n = 0;
 
 	for (i = 0; s[i]; i++)
-	{
-	if (s[i] == ' ')
-	{
-	if (s[i + 1] != ' ' && s[i + 1] != '\0')
+	{if (s[i] == ' ')
+	{if (s[i + 1] != ' ' && s[i + 1] != '\0')
 	n++;
-		}
-		else if (i == 0)
+		}else if (i == 0)
 			n++;
 	}
 	n++;
@@ -50,30 +47,23 @@ char **strtow(char *str)
 	w[n - 1] = NULL;
 	i = 0;
 	while (str[i])
-	{
-		if (str[i] != ' ' && (i == 0 || str[i - 1] == ' '))
-		{
-			for (j = 1; str[i + j] != ' ' && str[i + j]; j++)
-				;
+	{if (str[i] != ' ' && (i == 0 || str[i - 1] == ' '))
+		{for (j = 1; str[i + j] != ' ' && str[i + j]; j++);
 			j++;
 			w[wc] = (char *)malloc(j * sizeof(char));
 			j--;
 			if (w[wc] == NULL)
-			{
-				for (k = 0; k < wc; k++)
+			{for (k = 0; k < wc; k++)
 					free(w[k]);
 				free(w[n - 1]);
 				free(w);
 				return (NULL);
-			}
-			for (l = 0; l < j; l++)
+			}for (l = 0; l < j; l++)
 				w[wc][l] = str[i + l];
 			w[wc][l] = '\0';
 			wc++;
 			i += j;
-		}
-		else
-			i++;
+		}else i++;
 	}
 	return (w);
 }
