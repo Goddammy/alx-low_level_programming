4916a15bc36cc67ddbf180b4eda1f1cd2a41a886
goddammy <goddammy@gmail.com>
2022-04-12 10:03:05 -0700

    0-malloc_checked.c

--- 0x0C-more_malloc_free/main.h
+++ 0x0C-more_malloc_free/main.h
@@ -1,13 +1,9 @@
 #ifndef MAIN_H
-
 #define MAIN_H
-
 #include <stdlib.h>
-
 /**
- * Auth: Efa
- * Desc: Header file containing prototypes for all functions
- * used in the 0x0C-more_malloc_free directory.
+ *  Auth: Efa
+ *  Desc: Header file containing prototypes for all functions
  */
 
 int _putchar(char c);
