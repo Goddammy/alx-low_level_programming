c774a48acd956c62e6ce91ae790f45dd8f9f7d76
goddammy <goddammy@gmail.com>
2022-04-12 16:27:05 -0700

    dog.h

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,9 +1,7 @@
 #ifndef DOG_H
-
 #define DOG_H
 
 #include <stdio.h>
-
 #include <stdlib.h>
 
 /**
@@ -14,12 +12,9 @@
  */
 
 struct dog
-
 {
 	char *name;
-
 	float age;
-
 	char *owner;
 
 };
@@ -30,12 +25,10 @@ struct dog
 
 	typedef struct dog dog_t;
 
+
 	void init_dog(struct dog *d, char *name, float age, char *owner);
-
 	void print_dog(struct dog *d);
-
 	dog_t *new_dog(char *name, float age, char *owner);
-
 	void free_dog(dog_t *d);
 
 #endif /* DOG_H */
