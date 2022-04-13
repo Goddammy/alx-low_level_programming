b37bfb6abfc5b58b64d5c5ec1ddb48e20d0c8ee4
Goddammy <31579388+Goddammy@users.noreply.github.com>
2022-04-13 22:10:11 +0100

    Update 4-new_dog.c

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -3,28 +3,27 @@
 /**
  * _strdup - create a new array containing a copy of the given string
  * @str: a pointer to the string to copy
+ *
  * Return: NULL if str is NULL or if memory allocation fails,
  * otherwise a return a pointer to the new copy
  */
-
 char *_strdup(char *str)
-
 {
 	char *dup;
-
 	unsigned int size = 0;
 
 	if (str)
 	{
-	while (str[size++])
-	;
-	dup = malloc(sizeof(char) * size);
-	if (dup)
-	{
-	while (size--)
-	dup[size] = str[size];
-	return (dup);
-	}
+		while (str[size++])
+			;
+		dup = malloc(sizeof(char) * size);
+		if (dup)
+		{
+			while (size--)
+				dup[size] = str[size];
+
+			return (dup);
+		}
 	}
 	return (NULL);
 }
@@ -34,39 +33,43 @@ char *_strdup(char *str)
  * @name: the new dog's name
  * @age: the new dog's age
  * @owner: the new dog's owner
+ *
  * Return: a pointer to the new dog, or NULL if memory allocation fails
  */
-
 dog_t *new_dog(char *name, float age, char *owner)
-
 {
 	dog_t *d;
+
 	d = malloc(sizeof(dog_t));
 	if (!d)
-	return (NULL);
+		return (NULL);
+
 	if (name)
 	{
-	d->name = _strdup(name);
-	if (!(d->name))
-	{
-	free(d);
-	return (NULL);
-	}
+		d->name = _strdup(name);
+		if (!(d->name))
+		{
+			free(d);
+			return (NULL);
+		}
 	}
 	else
-	d->name = NULL;
+		d->name = NULL;
+
 	d->age = age;
+
 	if (owner)
 	{
-	d->owner = _strdup(owner);
-	if (!(d->owner))
-	{
-	free(d->name);
-	free(d);
-	return (NULL);
-	}
+		d->owner = _strdup(owner);
+		if (!(d->owner))
+		{
+			free(d->name);
+			free(d);
+			return (NULL);
+		}
 	}
 	else
-	d->owner = NULL;
+		d->owner = NULL;
+
 	return (d);
 }
